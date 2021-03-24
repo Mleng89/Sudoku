a361f6f19ca6673a9aafe1382bc748c593ddf0d5
Stephen Ro <stephenkro@gmail.com>
2021-03-24 16:54:42 -0400

    feat(added basic Game Board)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import {SudokuGrid} from './SudokuGrid'
 
 function App() {
+	
 	return (
 		<div className='App'>
 			<h1>Sudoku</h1>
+			<div className="sudoku-board"> 
+			<SudokuGrid />
+            <SudokuGrid/>
+			<SudokuGrid />
+            <SudokuGrid />
+			<SudokuGrid />
+			<SudokuGrid />
+			<SudokuGrid />
+			<SudokuGrid />
+			<SudokuGrid />
+           </div>
 		</div>
 	);
 }
---
+++ src/SudokuGrid.js
@@ -0,0 +1,25 @@
+
+export function SudokuGrid() {
+	return (
+		  <table className='sudoku-grid'>
+			<tr>
+				<td className="sudoku-cell"></td>
+				<td className="sudoku-cell">3</td>
+				<td className="sudoku-cell"></td>
+			</tr>
+			<tr>
+				<td className="sudoku-cell"></td>
+				<td className="sudoku-cell">6</td>
+				<td className="sudoku-cell"></td>
+			</tr>
+			<tr>
+				<td className="sudoku-cell"></td>
+				<td className="sudoku-cell">5</td>
+				<td className="sudoku-cell"></td>
+			</tr>
+
+			</table>
+		
+		
+	);
+}
