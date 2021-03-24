d8a4b1d3f6dcf22855568bd31217585647b6c125
Matthew Leng <mleng89@gmail.com>
2021-03-24 17:30:08 -0400

    feat(added number boxes under board)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import './App.css';
-import {SudokuGrid} from './SudokuGrid'
+import { SudokuGrid } from './SudokuGrid';
+import NumberButtons from './NumberButtons';
 
 function App() {
-	
 	return (
 		<div className='App'>
 			<h1>Sudoku</h1>
-			<div className="sudoku-board"> 
-			<SudokuGrid />
-            <SudokuGrid/>
-			<SudokuGrid />
-            <SudokuGrid />
-			<SudokuGrid />
-			<SudokuGrid />
-			<SudokuGrid />
-			<SudokuGrid />
-			<SudokuGrid />
-           </div>
+			<p>
+				<u>
+					<b>Description:</b>
+				</u>{' '}
+				Click on a number first before selecting which box in the Sudoku board
+				to put it in.
+			</p>
+			<div className='sudoku-board'>
+				<SudokuGrid />
+				<SudokuGrid />
+				<SudokuGrid />
+				<SudokuGrid />
+				<SudokuGrid />
+				<SudokuGrid />
+				<SudokuGrid />
+				<SudokuGrid />
+				<SudokuGrid />
+			</div>
+			<NumberButtons />
 		</div>
 	);
 }
---
+++ src/NumberButtons.js
@@ -0,0 +1,37 @@
+export default function NumberButton() {
+	return (
+		<div>
+			<table className='number_container'>
+				<tr>
+					<td>
+						<h2>1</h2>
+					</td>
+					<td>
+						<h2>2</h2>
+					</td>
+					<td>
+						<h2>3</h2>
+					</td>
+					<td>
+						<h2>4</h2>
+					</td>
+					<td>
+						<h2>5</h2>
+					</td>
+					<td>
+						<h2>6</h2>
+					</td>
+					<td>
+						<h2>7</h2>
+					</td>
+					<td>
+						<h2>8</h2>
+					</td>
+					<td>
+						<h2>9</h2>
+					</td>
+				</tr>
+			</table>
+		</div>
+	);
+}
