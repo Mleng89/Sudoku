33fe5e3ddeda95ca13990270d3f56e07263bee22
Matthew Leng <mleng89@gmail.com>
2021-03-26 19:16:06 -0400

    feat(added ability to store state in sudoku grid)

--- src/SudokuGrid.js
+++ src/SudokuGrid.js
@@ -3,24 +3,105 @@ import { useState } from 'react';
 export function SudokuGrid(props) {
 	const value = props.value;
 	console.log('what is value?', value);
-	const [cell, setCell] = useState('');
+
 	return (
 		<>
 			<table className='sudoku-grid'>
 				<tr>
-					<td className='sudoku-cell'></td>
-					<td className='sudoku-cell'></td>
-					<td className='sudoku-cell'></td>
+					<td className='sudoku-cell'>
+						<button
+							onClick={(e) => {
+								e.target.innerText = value;
+								console.log(e.target.innerText);
+							}}
+						>
+							_
+						</button>
+					</td>
+					<td className='sudoku-cell'>
+						<button
+							onClick={(e) => {
+								e.target.innerText = value;
+								console.log(e.target.innerText);
+							}}
+						>
+							_
+						</button>
+					</td>
+					<td className='sudoku-cell'>
+						<button
+							onClick={(e) => {
+								e.target.innerText = value;
+								console.log(e.target.innerText);
+							}}
+						>
+							_
+						</button>
+					</td>
 				</tr>
 				<tr>
-					<td className='sudoku-cell'></td>
-					<td className='sudoku-cell'></td>
-					<td className='sudoku-cell'></td>
+					<td className='sudoku-cell'>
+						<button
+							onClick={(e) => {
+								e.target.innerText = value;
+								console.log(e.target.innerText);
+							}}
+						>
+							_
+						</button>
+					</td>
+					<td className='sudoku-cell'>
+						<button
+							onClick={(e) => {
+								e.target.innerText = value;
+								console.log(e.target.innerText);
+							}}
+						>
+							_
+						</button>
+					</td>
+					<td className='sudoku-cell'>
+						<button
+							onClick={(e) => {
+								e.target.innerText = value;
+								console.log(e.target.innerText);
+							}}
+						>
+							_
+						</button>
+					</td>
 				</tr>
 				<tr>
-					<td className='sudoku-cell'></td>
-					<td className='sudoku-cell'></td>
-					<td className='sudoku-cell'></td>
+					<td className='sudoku-cell'>
+						<button
+							onClick={(e) => {
+								e.target.innerText = value;
+								console.log(e.target.innerText);
+							}}
+						>
+							_
+						</button>
+					</td>
+					<td className='sudoku-cell'>
+						<button
+							onClick={(e) => {
+								e.target.innerText = value;
+								console.log(e.target.innerText);
+							}}
+						>
+							_
+						</button>
+					</td>
+					<td className='sudoku-cell'>
+						<button
+							onClick={(e) => {
+								e.target.innerText = value;
+								console.log(e.target.innerText);
+							}}
+						>
+							_
+						</button>
+					</td>
 				</tr>
 			</table>
 		</>
